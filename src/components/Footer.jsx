4719f923584e91ef9d11016a462b4aff107ec5a6
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-04-03 10:36:34 -0400

    added blur effect to svg component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import { Box, Flex } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box padding={5} align='center'>
-        <Flex justifyContent='space-around' w='50%' bg='black'>
-            <Box bg='black' color='white'>ICON</Box>
-            <Box bg='black' color='white'>ICON</Box>
-            <Box bg='black' color='white'>ICON</Box>
+        <Flex justifyContent='space-around' w='50%'>
+            <Box>ICON</Box>
+            <Box>ICON</Box>
+            <Box>ICON</Box>
         </Flex>
     </Box>
   )
--- src/components/Triangle.jsx
+++ src/components/Triangle.jsx
@@ -10,9 +10,9 @@ const Triangle = () => {
           style={{ width: "100%", height: "auto" }}
         >
           {/* Outer triangle */}
-          <polygon points="100,10 200,190 0,190" style={{ fill: "#e4e4e4" }} />
+          <polygon points="100,10 200,190 0,190" style={{ fill: "#e4e4e4", filter: 'blur(20px)' }} />
           {/* Middle triangle */}
-          <polygon points="100,50 160,160 40,160" style={{ fill: "#eeeeee" }} />
+          <polygon points="100,50 160,160 40,160" style={{ fill: "#eeeeee",  filter: 'blur(8px)' }} />
           {/* Inner triangle */}
           <polygon points="100,80 130,135 70,135" style={{ fill: "#ffffff" }} />
         </svg>
