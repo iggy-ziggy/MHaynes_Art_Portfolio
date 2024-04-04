605ab90deea0da006890662b7a6d9affaa3f5385
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-04-04 17:12:23 -0400

    minor tweaks

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <NavBar />
-      <Triangle />
+      {/* <Triangle /> */}
       {/* <App /> */}
       <HomePage />
       <Footer />
