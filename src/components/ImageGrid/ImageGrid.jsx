4c009bc086259d029766997314095b0f83e245de
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-04-08 11:41:05 -0400

    refactored ImageGrid component to be reusable with other functions

--- src/components/ImageGrid/ImageGrid.jsx
+++ src/components/ImageGrid/ImageGrid.jsx
@@ -1,13 +1,17 @@
 import { Flex, GridItem, Image, SimpleGrid, Text } from "@chakra-ui/react";
-import useRandomImages from "../../hooks/useRandomImages";
+import PropTypes from "prop-types";
 import "./imageGrid.css";
 
-const ImageGrid = () => {
-  const data = useRandomImages();
+const ImageGrid = ({ action }) => {
+  const data = action();
 
   return (
-    <Flex justify='center'>
-      <SimpleGrid columns={{sm: 1, md: 2, lg: 3}} spacing={10} className="home_grid">
+    <Flex justify="center">
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3 }}
+        spacing={10}
+        className="home_grid"
+      >
         {data.map((img) => (
           <GridItem key={img.title} className="image_container">
             <Image src={img.url} />
@@ -17,6 +21,10 @@ const ImageGrid = () => {
       </SimpleGrid>
     </Flex>
   );
+};
+
+ImageGrid.propTypes = {
+  action: PropTypes.func.isRequired,
 };
 
 export default ImageGrid;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,14 @@ import Quote from "../components/Quote/Quote";
 import TextContent from "../components/TextContent/TextContent";
 import ImageGrid from "../components/ImageGrid/ImageGrid";
 import aboutMe from "../data/about";
+import useRandomImages from "../hooks/useRandomImages";
 
 const HomePage = () => {
   return (
     <>
       <Quote />
       <TextContent content={aboutMe}/>
-      <ImageGrid />
+      <ImageGrid action={useRandomImages}/>
     </>
   );
 };
