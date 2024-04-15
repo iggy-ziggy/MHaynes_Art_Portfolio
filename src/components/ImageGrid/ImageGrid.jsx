0c5eea2025fa1f86a03e717e34be31af425315d2
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-04-15 13:02:02 -0400

    added loading logic to ImageGrid.jsx

--- src/components/ImageGrid/ImageGrid.jsx
+++ src/components/ImageGrid/ImageGrid.jsx
@@ -2,28 +2,46 @@ import { Flex, GridItem, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import "./imageGrid.css";
+import { useEffect, useState } from "react";
 
 const ImageGrid = ({ data }) => {
-  const collection = data;
+  const images = data;
+  const [loadingStatus, setLoadingStatus] = useState({})
+  const [allImagesLoaded, setAllImagesLoaded] = useState(false);
 
-  return (
-    <Flex mt='4rem' justify="center">
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3 }}
-        spacing={2}
-        className="image_grid"
-      >
-        {collection.map((img) => (
-          <GridItem key={img.id} className="image_container">
-            <Link to={`/images/${img.id}`}>
-              <Image src={img.url} className="thumbnail" />
-            </Link>
-            <Text className="image_title">{img.title}</Text>
-          </GridItem>
-        ))}
-      </SimpleGrid>
-    </Flex>
-  );
+  useEffect(() => {
+    const allLoaded = Object.values(loadingStatus).every(status => status === true)
+    setAllImagesLoaded(allLoaded)
+  }, [loadingStatus])
+
+  const handleLoad = (imgId) => {
+    setLoadingStatus(prevStatus => ({
+      ...prevStatus,
+      [imgId]: true
+    }))
+    // console.log(`Image with ID ${imgId} has finished loading`)
+  }
+
+  
+    return (
+      <Flex mt='4rem' justify="center">
+        <SimpleGrid
+          columns={{ sm: 1, md: 2, lg: 3 }}
+          spacing={2}
+          className="image_grid"
+        >
+          {allImagesLoaded ? images.map((img) => (
+            <GridItem key={img.id} className="image_container">
+              <Link to={`/images/${img.id}`}>
+                <Image src={img.url} className="thumbnail" onLoad={() => handleLoad(img.id)} />
+              </Link>
+              <Text className="image_title">{img.title}</Text>
+            </GridItem>
+          )) : null}
+        </SimpleGrid>
+      </Flex>
+    );
+  
 };
 
 ImageGrid.propTypes = {
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,10 +4,28 @@ import Contact from "../components/Contact/Contact";
 const ContactPage = () => {
   return (
     <>
-      <VStack mt='5rem'>
-        <Center w='60%' style={{color: 'var(--lightGrey)', fontWeight: '600', fontSize: '1.2rem'}}>Please feel free to ask me whatever is on your mind.</Center>
-        <Center w='60%' style={{color: 'var(--lightGrey)', fontWeight: '600'}}>In addition to the work featured on the site, I also wrote the code for the site itself. If you experience any bugs or have suggestions for a better user experience, please let me know.</Center>
-        <Center style={{color: 'var(--lightGrey)', fontSize: '2rem'}}>. . .</Center>
+      <VStack mt="5rem">
+        <Center
+          w="60%"
+          style={{
+            color: "var(--lightGrey)",
+            fontWeight: "600",
+            fontSize: "1.2rem",
+          }}
+        >
+          Please feel free to ask me whatever is on your mind.
+        </Center>
+        <Center
+          w="60%"
+          style={{ color: "var(--lightGrey)", fontWeight: "600" }}
+        >
+          In addition to the work featured on the site, I also wrote the code
+          for the site itself. If you experience any bugs or have suggestions
+          for a better user experience, please let me know.
+        </Center>
+        <Center style={{ color: "var(--lightGrey)", fontSize: "2rem" }}>
+          . . .
+        </Center>
       </VStack>
       <Contact />
     </>
