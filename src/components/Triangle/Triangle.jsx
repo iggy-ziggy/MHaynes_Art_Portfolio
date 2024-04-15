80d9200575261aa5efe85c4af746f1d3cb110cd1
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-04-15 18:24:20 -0400

    svg filter styles not showing up on mobile -attempting to fix

--- src/components/Triangle/Triangle.jsx
+++ src/components/Triangle/Triangle.jsx
@@ -1,33 +1,21 @@
 import { Box } from "@chakra-ui/react";
-import './triangle.css';
-
-const color1 = '#c6cccb';
-const color2 = '#e0dfd5';
-const color2alt = '#dfe6e5';
-const color3 = '#fcfbf0';
-const color4 = '#e4e4e4';
-const color5 = '#eeeeee';
-const color6 = '#ffffff';
-
-
+import "./triangle.css";
 
 const Triangle = () => {
-    
   return (
     <Box className="triangle_container">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="triangle"
-          viewBox="0 0 200 200"
-          style={{ width: "100%" }}
-        >
-          {/* Outer triangle */}
-          <polygon points="100,-20 200,170 0,170" style={{ fill: color1, filter: 'blur(35px)' }} />
-          {/* Middle triangle */}
-          <polygon points="100,20 160,140 40,140" style={{ fill: color2alt,  filter: 'blur(4px)' }} />
-          {/* Inner triangle */}
-          <polygon points="100,50 150,135 50,135" style={{ fill: color6 }} />
-        </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="triangle"
+        viewBox="0 0 200 200"
+        style={{ width: "100%" }}
+      >
+        <polygon points="100,-20 200,170 0,170" className="triangle_outer" />
+
+        <polygon points="100,20 160,140 40,140" className="triangle_middle" />
+
+        <polygon points="100,50 150,135 50,135" className="triangle_inner" />
+      </svg>
     </Box>
   );
 };
