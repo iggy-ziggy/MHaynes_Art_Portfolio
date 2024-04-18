2d62bd2fafe871adfb091babc3040fbd7a6b13f1
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-04-18 16:22:08 -0400

    refactored ImageGrid to be a masonry grid with Responsive Masonry Grid and React Measure

--- src/components/ImageGrid/ImageGrid.jsx
+++ src/components/ImageGrid/ImageGrid.jsx
@@ -1,47 +1,58 @@
-import { Flex, GridItem, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import Measure from "react-measure";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import { Link } from "react-router-dom";
+import { Text } from "@chakra-ui/react";
 import "./imageGrid.css";
-import { useEffect, useState } from "react";
 
 const ImageGrid = ({ data }) => {
   const images = data;
-  const [loadingStatus, setLoadingStatus] = useState({})
+  // console.log(images);
+
+  const [loadingStatus, setLoadingStatus] = useState({});
   const [allImagesLoaded, setAllImagesLoaded] = useState(false);
 
   useEffect(() => {
-    const allLoaded = Object.values(loadingStatus).every(status => status === true)
-    setAllImagesLoaded(allLoaded)
-  }, [loadingStatus])
+    const allLoaded = Object.values(loadingStatus).every(
+      (status) => status === true
+    );
+    setAllImagesLoaded(allLoaded);
+  }, [loadingStatus]);
 
   const handleLoad = (imgId) => {
-    setLoadingStatus(prevStatus => ({
+    setLoadingStatus((prevStatus) => ({
       ...prevStatus,
-      [imgId]: true
-    }))
+      [imgId]: true,
+    }));
     // console.log(`Image with ID ${imgId} has finished loading`)
-  }
+  };
 
-  
-    return (
-      <Flex mt='4rem' justify="center">
-        <SimpleGrid
-          columns={{ sm: 1, md: 2, lg: 3 }}
-          spacing={2}
-          className="image_grid"
-        >
-          {allImagesLoaded ? images.map((img) => (
-            <GridItem key={img.id} className="image_container">
-              <Link to={`/images/${img.id}`}>
-                <Image src={img.url} className="thumbnail" onLoad={() => handleLoad(img.id)} />
-              </Link>
-              <Text className="image_title">{img.title}</Text>
-            </GridItem>
-          )) : null}
-        </SimpleGrid>
-      </Flex>
-    );
-  
+  return (
+    <div className="image_grid">
+      <ResponsiveMasonry columnsCountBreakPoints={{ 500: 1, 700: 2, 900: 3 }}>
+        <Masonry columnsCount={3} gutter="0.5rem">
+          {allImagesLoaded &&
+            images.map((image, index) => (
+              <Measure key={index}>
+                {({ measureRef }) => (
+                  <div className="grid_item">
+                    <Link to={`/images/${image.id}`}>
+                      <img
+                        src={image.url}
+                        ref={measureRef}
+                        onLoad={() => handleLoad(image.id)}
+                      />
+                    </Link>
+                    <Text className="image_title">{image.title}</Text>
+                  </div>
+                )}
+              </Measure>
+            ))}
+        </Masonry>
+      </ResponsiveMasonry>
+    </div>
+  );
 };
 
 ImageGrid.propTypes = {
@@ -49,3 +60,22 @@ ImageGrid.propTypes = {
 };
 
 export default ImageGrid;
+
+// return (
+//   <Flex mt='4rem' justify="center">
+//     <SimpleGrid
+//       columns={{ sm: 1, md: 2, lg: 3 }}
+//       spacing={2}
+//       className="image_grid"
+//     >
+//       { images.map((img) => (
+//         <GridItem key={img.id} className="image_container">
+//           <Link to={`/images/${img.id}`}>
+//             <Image src={img.url} className="thumbnail"  />
+//           </Link>
+//           <Text className="image_title">{img.title}</Text>
+//         </GridItem>
+//       )) : null}
+//     </SimpleGrid>
+//   </Flex>
+// );
