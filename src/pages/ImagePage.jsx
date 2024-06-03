f3e612f840d16b8d50ead5c3a8ec92d6573f9c78
Matt Haynes <128410000+iggy-ziggy@users.noreply.github.com>
2024-06-03 18:10:53 -0400

    changed source of image files, added link to print shop for available pieces, and added descriptions to certain collection pages

--- src/pages/ImagePage.jsx
+++ src/pages/ImagePage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { Container } from "@chakra-ui/react";
+import { Button, Container } from "@chakra-ui/react";
 import ImageDetails from "../components/ImageDetails/ImageDetails";
 import SingleImage from "../components/SingleImage/SingleImage";
 import useImage from "../hooks/useImage";
@@ -9,13 +9,24 @@ const ImagePage = () => {
   const image = useImage(parseInt(id));
 
   return (
-    <Container mt='3rem' mb='3rem'>
+    <Container mt="3rem" mb="3rem">
       <SingleImage url={image.url} />
       <ImageDetails
         title={image.title}
         media={image.media}
         date={image.year_completed}
       />
+      {image.prints_available && (
+        <Button className="gold_btn">
+          <a
+            href={image.prints_available}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Prints Available
+          </a>
+        </Button>
+      )}
     </Container>
   );
 };
